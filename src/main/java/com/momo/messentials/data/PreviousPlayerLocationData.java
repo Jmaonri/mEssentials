47b8fe4ab02e5c0bb766b7fe0fd8180412acf975
Jmaonri Covarrubias <jmaonri@gmail.com>
2025-04-11 19:07:20 -0600

    renamed file

--- src/main/java/com/momo/messentials/data/PreviousPlayerLocationData.java
+++ src/main/java/com/momo/messentials/data/PreviousPlayerLocationData.java
@@ -1,11 +1,11 @@
 package com.momo.messentials.data;
 
-public class LastLocationData {
+public class PreviousPlayerLocationData {
     public int x, y, z;
     public String dimension;
     public float yaw,pitch;
 
-    public LastLocationData(int x, int y, int z, String dimension, float yaw, float pitch) {
+    public PreviousPlayerLocationData(int x, int y, int z, String dimension, float yaw, float pitch) {
         this.x = x;
         this.y = y;
         this.z = z;
